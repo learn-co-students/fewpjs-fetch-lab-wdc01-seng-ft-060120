cb3c3f59db66add18123a6ed1c0308483930d640
Catrina Friday <cat.friday@gmail.com>
2020-07-15 21:24:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// function fetchBooks() {
-//   return fetch('https://anapioficeandfire.com/api/books')
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(json){
-//     renderBooks
-//     console.log(renderBooks)
-//   })
-// }
 function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books")
-    .then(resp => resp.json())
-    .then(json => renderBooks(json))
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json){
+    renderBooks(json)
+    console.log(renderBooks)
+  })
 }
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     .then(resp => resp.json())
+//     .then(json => renderBooks(json))
+// }
 
 
 function renderBooks(books) {
